b586393e641b1fb0e9bf4792c8c29a0b208daff7
Mabi19 <mateuszbartnik33@gmail.com>
2023-11-28 22:32:25 +0100

    Add hero section

--- _config.ts
+++ _config.ts
@@ -1,8 +1,11 @@
 import lume from "lume/mod.ts";
+import postcss from "lume/plugins/postcss.ts";
 
 const site = lume({
     location: new URL("https://mabi.tmpinc.io"),
 });
+
+site.use(postcss());
 
 site.copy("assets");
 
