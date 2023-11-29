a465725a51477eba1c5929455c0a3a284422b6c2
Mabi19 <mateuszbartnik33@gmail.com>
2023-11-29 20:04:08 +0100

    Add data-driven projects

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
+import toml from "lume/plugins/toml.ts";
 
 const site = lume({
     location: new URL("https://mabi.tmpinc.io"),
 });
 
 site.use(postcss());
+site.use(toml());
 
 site.copy("assets");
 
