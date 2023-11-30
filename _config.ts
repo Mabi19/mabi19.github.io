ab5e42ad71297645cc36b782a54a9e571063a869
Mabi19 <mateuszbartnik33@gmail.com>
2023-11-30 19:40:39 +0100

    ⚡️ Add minifier plugins

--- _config.ts
+++ _config.ts
@@ -1,13 +1,18 @@
 import lume from "lume/mod.ts";
-import postcss from "lume/plugins/postcss.ts";
 import toml from "lume/plugins/toml.ts";
+import postcss from "lume/plugins/postcss.ts";
+import minifyHTML from "lume/plugins/minify_html.ts";
+import lightningCSS from "lume/plugins/lightningcss.ts";
 
 const site = lume({
     location: new URL("https://mabi.tmpinc.io"),
 });
 
-site.use(postcss());
 site.use(toml());
+
+site.use(postcss());
+site.use(minifyHTML());
+site.use(lightningCSS());
 
 site.copy("assets");
 
