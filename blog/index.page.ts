75848048c87925dab3a5dcea9e74da18ae623e20
Mabi19 <mateuszbartnik33@gmail.com>
2024-07-26 23:00:52 +0200

    🎨 Use lists for head_extra
    This allows for merging it

--- blog/index.page.ts
+++ blog/index.page.ts
@@ -1,7 +1,7 @@
 import { type PageData, html } from "../_includes/utils.ts";
 
 export const layout = "simple.vto";
-export const head_extra = `<link rel="stylesheet" href="/styles/blog.css">`;
+export const head_extra = [`<link rel="stylesheet" href="/styles/blog.css">`];
 
 const postList = (posts: PageData[]) => html`
     <ol class="post-list">
